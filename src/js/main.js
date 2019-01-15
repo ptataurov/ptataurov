9458d67d3f08d68eb97f0b5362e85e5efe92db35
Pavel Tataurov <p.tataurov92@gmail.com>
2019-01-15 21:49:49 +0300

    deleted jquery and refactored code on pure js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-import $ from 'jquery/dist/jquery';
 import FullPage from 'fullpage.js/dist/fullpage';
 
 window.onload = () => {
@@ -27,41 +26,68 @@ window.onload = () => {
   }
 };
 
-$(document).ready(function() {
-
+document.addEventListener("DOMContentLoaded", () => {
   new FullPage('#fullpage', {
     dragAndMove: true
   });
 
-  const home = () => {
-    follower = $('.cursor-follower'), cursor = $('.cursor'), cursorfollow(), $(document).on("mousemove", e => {
-        mouseX = e.pageX, mouseY = e.pageY;
-    })
+  const start = () => {
+
+    follower = document.querySelector('.cursor-follower');
+    cursor = document.querySelector('.cursor');
+
+    cursorfollow();
+
+    document.addEventListener('mousemove', e => {
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+    });
   }
 
   const cursorfollow = () => {
-    let e = 0,
-        s = 0;
+
+    let e = 0, s = 0;
+
     setInterval(() => {
-      e += (mouseX - e) / 9, s += (mouseY - s) / 9, follower.css({
-          left: e - 12,
-          top: s - 12
-      }), cursor.css({
-          left: mouseX,
-          top: mouseY
-      })
+
+      e += (mouseX - e) / 9;
+      s += (mouseY - s) / 9;
+
+      follower.style.left = `${e - 12}px`;
+      follower.style.top = `${s - 12}px`;
+
+
+
+      cursor.style.left = `${mouseX}px`;
+      cursor.style.top = `${mouseY}px`;
+
     }, 16);
 
-    $('.cur').on('mouseenter', function() {
-      follower.addClass('active'), cursor.addClass('active'), $('.hover').addClass('hovered'), $(this).removeClass('hovered')
-    }), $('.cur').on('mouseleave', function() {
-      follower.removeClass('active'), cursor.removeClass('active'), $('.hover').removeClass('hovered')
-    })
+    const cur = document.querySelectorAll('.cur');
+    const hover = document.querySelectorAll('.hover');
+
+
+    cur.forEach(el => {
+      el.addEventListener('mouseenter', function(e) {
+
+        follower.classList.add('active');
+        cursor.classList.add('active');
+        hover.forEach(el => el.classList.add('hovered'));
+        this.classList.remove('hovered');
+      });
+
+      el.addEventListener('mouseleave', function() {
+        follower.classList.remove('active');
+        cursor.classList.remove('active');
+        hover.forEach(el => el.classList.remove('hovered'));
+      });
+    });
   }
 
   let mouseX = 0,
-      mouseY = 0,
-      follower, cursor = !1;
+    mouseY = 0,
+    follower,
+    cursor = !1;
 
-  home();
+  start();
 });
